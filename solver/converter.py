8f91f933a7a5279964da023779c7368bb4f8183f
Eason Liu <yichian.liu@gmail.com>
2022-01-22 10:58:10 -0800

    Complte simple dedup method

--- solver/converter.py
+++ solver/converter.py
@@ -5,8 +5,12 @@ import pandas as pd
 
 
 class DealConverter:
+    card: pd.DataFrame
+    card_: pd.DataFrame
+
     def __init__(self):
         self.card = None
+        self.card_ = None
 
     def read_yolo(self, path):
         with open(path, 'r') as f:
@@ -14,6 +18,21 @@ class DealConverter:
         self.card = pd.json_normalize(yolo_json[0]['objects'])
 
     def dedup(self):
+        self.card_ = self._dedup_simple()
+
+    def _dedup_simple(self):
+        """Dedup in a simple way, only keeping the one with highest confidence."""
+        return (self.card
+                    .sort_values('confidence')
+                    .drop_duplicates(subset='name', keep='last'))
+
+    def _dedup_smart(self):
+        # TODO
+        # rule:
+        # 1. find out dup pairs whose dist between a range: not too far nor too close
+        # 2. reomve from each pair:
+        #     1) the one with lower conf, if conf diff to much
+        #     2) the onw closer to image center, otherwise
         pass
 
     # three cases after dedup
--- solver/tests.py
+++ solver/tests.py
@@ -34,3 +34,12 @@ class TestConverter:
         assert converter.card.shape == (51, 7)
         assert 'name' in converter.card.columns
         assert 'relative_coordinates.center_x' in converter.card.columns
+
+    def test_simple_dedup(self, converter):
+        converter.read_yolo(self.YOLO_FILEPATH)
+        converter.dedup()
+
+        assert isinstance(converter.card_, pd.DataFrame)
+        assert converter.card_.shape == (41, 7)
+        assert converter.card_.name.value_counts().max() == 1
+        assert converter.card_.query('name == "5c"').confidence.iloc[0] == 0.999394
