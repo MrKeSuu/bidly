0517cca0c3e7a6b27123c63fb4bc1db90eaec304
Eason Liu <yichian.liu@gmail.com>
2022-02-06 09:49:27 -0800

    Partially complete method: report_missing

--- solver/converter.py
+++ solver/converter.py
@@ -4,6 +4,14 @@ import json
 import pandas as pd
 
 
+CARD_CLASSES = [
+    '2s', '3s', '4s', '5s', '6s', '7s', '8s', '9s', '10s', 'Js', 'Qs', 'Ks', 'As',
+    '2c', '3c', '4c', '5c', '6c', '7c', '8c', '9c', '10c', 'Jc', 'Qc', 'Kc', 'Ac',
+    '2d', '3d', '4d', '5d', '6d', '7d', '8d', '9d', '10d', 'Jd', 'Qd', 'Kd', 'Ad',
+    '2h', '3h', '4h', '5h', '6h', '7h', '8h', '9h', '10h', 'Jh', 'Qh', 'Kh', 'Ah'
+]
+
+
 class DealConverter:
     card: pd.DataFrame
     card_: pd.DataFrame
@@ -15,7 +23,7 @@ class DealConverter:
     def read_yolo(self, path):
         with open(path, 'r') as f:
             yolo_json = json.load(f)
-        self.card = pd.json_normalize(yolo_json[0]['objects'])
+        self.card = pd.json_normalize(yolo_json[0]['objects'])  # image has one frame only
 
     def dedup(self):
         self.card_ = self._dedup_simple()
@@ -37,16 +45,21 @@ class DealConverter:
 
     # three cases after dedup
     def assign(self):
-        """Case 1: everything is perfect -> work on assign cards to four hands"""
+        """Case 1: everything is perfect -> work on assigning cards to four hands"""
         pass
 
     def report_missing_and_fp(self):
         """Case 2: missing cards or FP cards -> report them"""
-        pass
+        # report missing
+        detected_classes = set(self.card_.name)
+        missing_classes = [name for name in CARD_CLASSES if name not in detected_classes]
+        print('Missing cards:', missing_classes)
+
+        # report FP  TODO
 
     def infer_missing(self):
         """Case 3: missing cards -> attempt to infer"""
-        pass  # * lower priority
+        pass  # *lower priority
 
     def write_pbn(self, path):
         pass
