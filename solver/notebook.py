8c585276ddd341942f7a617847c8d1722b63bd29
Eason Liu <yichian.liu@gmail.com>
2022-03-04 11:29:14 -0800

    Rename things in notebook

--- solver/notebook.py
+++ solver/notebook.py
@@ -56,19 +56,19 @@ converter.report_missing_and_fp()
 # %%
 def read_yolo(path):
     with open(path) as f:
-        return pd.json_normalize(json.load(f)[0]['objects'], sep='__')
+        return pd.json_normalize(json.load(f)[0]['objects'])
 
-res = read_yolo(YOLO_JSON_FILEPATH_3S)
+res = read_yolo(YOLO_JSON_FILEPATH_1M)
 res.shape
 
 
 # %%
 def _make_pair_wise(df: pd.DataFrame):
     midx = pd.MultiIndex.from_product([df.index, df.index], names=['n1', 'n2'])
-    dist = pd.concat(
+    pair = pd.concat(
         [df.add_suffix('_1').reindex(midx, level='n1'),
          df.add_suffix('_2').reindex(midx, level='n2')], axis=1)
-    return dist.loc[
+    return pair.loc[
         midx[midx.get_level_values(0) < midx.get_level_values(1)]]
 
 def _euclidean_dist(x1, y1, x2, y2):
@@ -76,9 +76,10 @@ def _euclidean_dist(x1, y1, x2, y2):
 
 
 # %%
-pair = (
+# calc pair dist
+dist = (
     res[['name', 'confidence',
-         '__relative_coordinates__center_x', '__relative_coordinates__center_y']]
+         'relative_coordinates.center_x', 'relative_coordinates.center_y']]
         .rename(columns=lambda s: s.split('_')[-1])
         .query('confidence >= 0.7')  # debug
         # make uniq names for pair-wise dists
@@ -90,17 +91,17 @@ pair = (
                         .cat([df.group_rank.astype(int).astype(str)], sep='_'))
         .drop(columns=['group_rank']).set_index('uniq_name')
         .pipe(_make_pair_wise)
-        .query('name_1 == name_2').drop(columns=['name_1', 'name_2'])
+        .query('name_1 == name_2')
         .assign(dist_=lambda df: df.apply(
             lambda df: _euclidean_dist(df.x_1, df.y_1, df.x_2, df.y_2),
             axis=1))
         .sort_index()
 )
-pair.shape
+dist.shape
 
 # %%
 X_dist = (
-    pair.query('0.1 <= dist_ <= 0.3')
+    dist.query('0.1 <= dist_ <= 0.3')
         .dist_
         .values.reshape(-1, 1)
 )
@@ -114,6 +115,7 @@ def find_densest(dists, min_size):
 
     As part of smart dedup, the idea is to find the 'mode' of
     all pair-wise distances, in order to filter out bad pairs.
+
     - `eps` tuned for dist between two symbols on the same card
     - only works for 1-d array currently"""
     X = np.array(dists).reshape(-1, 1)
@@ -142,8 +144,8 @@ def locate_detected_classes(res, min_conf=0.7):
             continue
         ax.annotate(
             row['name'],
-            (row['__relative_coordinates__center_x'],
-             1-row['__relative_coordinates__center_y']))
+            (row['relative_coordinates.center_x'],
+             1-row['relative_coordinates.center_y']))
 
 
 # %%
