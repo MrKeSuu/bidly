b6daf45f79e923fd03a37cf239f8a5e0d04a362b
Eason Liu <yichian.liu@gmail.com>
2023-07-26 07:29:12 -0700

    Simple solution for adjustable popup size with line wrap

--- app/ui.py
+++ app/ui.py
@@ -5,20 +5,22 @@ from kivy.uix.popup import Popup
 from kivy.uix.boxlayout import BoxLayout
 
 
+class PopupLabel(Label):
+    pass
+
+
 def popup(title, msg, close_btn=False, auto_dismiss=False):
     content = BoxLayout(orientation='vertical')
 
-    label = Label(text=msg, text_size=(800, None))
-    label.bind(size=lambda ins: ins.texture_size)
-    content.add_widget(Label(text=msg))
-
+    label = PopupLabel(text=msg, valign='center', halign='center')
+    content.add_widget(label)
     if close_btn:
-        button = Button(text='Close')
+        button = Button(text='Close', size_hint=(1, None))
         content.add_widget(button)
 
     popup = Popup(
         title=title, content=content,
-        size_hint=(0.9, None), height=400, auto_dismiss=auto_dismiss)
+        size_hint=(.8, .3), auto_dismiss=auto_dismiss)
     popup.open()
 
     if close_btn:
